2927b8981f46e113ea96d300e3979fc6cfd7b0c2
grace <gracechongbl@gmail.com>
2020-12-01 13:11:17 +0800

    added clicker functionality

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    console.log(`Count before incrementing is ${count}`);
+    setCount(count + 1);
+    console.log(`Count after incrementing is ${count}`);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>{count}</Text>      
+      <Button title ="Presse Moi!" onPress={increment}></Button>
+      <Text>This is my amazing new App</Text>
       <StatusBar style="auto" />
     </View>
   );
